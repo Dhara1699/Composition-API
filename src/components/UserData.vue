fb1eb739e0fbc3d61ba37af04dae5579f2dd5576
Dhara <dhara1.patel@bacancy.com>
2021-03-17 11:40:04 +0530

    Commited!

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,26 +1,58 @@
 <template>
-    <section>
-        <h2>{{ userName }}</h2>
-    </section>
+  <section>
+    <h2>{{ userName }}</h2>
+  </section>
 </template>
 
 <script>
-import { computed } from 'vue';
+import {
+  computed,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
 export default {
-    props: ['firstName' , 'lastName'],
+  props: ['firstName', 'lastName'],
 
-    setup(props) {
-        const uName = computed(() => {
-            return props.firstName + ' ' + props.lastName;
-        });
+  setup(props) {
+    const uName = computed(() => {
+      return props.firstName + ' ' + props.lastName;
+    });
 
-        return { userName: uName };
-    }
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
 
-   /* computed: {
+    onMounted(() => {
+      console.log('onMounted');
+    });
+
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    });
+
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    });
+
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
+
+    return { userName: uName };
+  },
+
+  /* computed: {
         userName() {
             return this.firstName + ' ' + this.lastName;
         }
     }*/
-}
+};
 </script>
